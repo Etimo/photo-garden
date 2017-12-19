dd24fff8b43116c095fb52f0ee53822cfed8714f
Oscar Nylander <oscar.nylander@etimo.se>
2017-12-19 15:39:00 +0100

    #6: Trial and error

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd search-api
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    ./bin/ecs-deploy -c ${TASK_DEFINITION} -n ${SERVICE} -i ${REMOTE_IMAGE_URL}:latest
+    ./bin/ecs-deploy.sh -c ${TASK_DEFINITION} -n ${SERVICE} -i ${REMOTE_IMAGE_URL}:latest
   else
     echo "Not master, not deploying"
   fi
