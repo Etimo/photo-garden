c1c2ef4f7315a6e7b7ea160345a26fede9b3166f
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2017-12-19 14:58:39 +0100

    #9 test 2nd commit on issue

--- search-api/lib/setup/unleash.js
+++ search-api/lib/setup/unleash.js
@@ -17,7 +17,6 @@ function init() {
   // instance.on("error", console.error);
   // instance.on("warn", console.warn);
   // instance.on("ready", console.log);
-
   // metrics hooks
   // instance.on("registered", clientData => console.log("registered", clientData));
   // instance.on("sent", payload => console.log("metrics bucket/payload sent", payload));
