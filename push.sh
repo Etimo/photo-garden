dd5e45133b1225bc01dfebeadd3ea269d8898d5d
Oscar Nylander <oscar.nylander@etimo.se>
2017-12-19 13:58:30 +0100

    no include email

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; t
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
     pip install --user awscli
     export PATH=${PATH}:${HOME}/.local/bin
-    eval $(aws ecr get-login --region eu-west-1)
+    eval $(aws ecr get-login --no-include-email --region eu-west-1)
     docker tag photo-garden-search-api:latest \
         255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api:latest
     docker push 255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api:latest
