389fb3ed464686bf28decc106dc924d70ba4753b
Oscar Nylander <oscar.nylander@etimo.se>
2017-12-19 14:03:01 +0100

    new ecr repo

--- push.sh
+++ push.sh
@@ -4,8 +4,8 @@ if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; t
     export PATH=${PATH}:${HOME}/.local/bin
     eval $(aws ecr get-login --no-include-email --region eu-west-1)
     docker tag photo-garden-search-api:latest \
-        255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api:latest
-    docker push 255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api:latest
+         255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api 
+    docker push  255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api 
   else
     echo "Not master, not deploying"
   fi
