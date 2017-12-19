b36e307e22e9678cd5c15d4f71dee7eb54d0e876
Oscar Nylander <oscar.nylander@etimo.se>
2017-12-19 15:32:19 +0100

    Time to test the automatic deploying

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-echo "not implemented yet"
-exit 0
-
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
     ./bin/ecs-deploy -c ${TASK_DEFINITION} -n ${SERVICE} -i ${REMOTE_IMAGE_URL}:latest
--- push.sh
+++ push.sh
@@ -3,9 +3,8 @@ if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; t
     pip install --user awscli
     export PATH=${PATH}:${HOME}/.local/bin
     eval $(aws ecr get-login --no-include-email --region eu-west-1)
-    docker tag photo-garden-search-api:latest \
-         255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api 
-    docker push  255196862254.dkr.ecr.eu-west-1.amazonaws.com/etimo/photo-garden-search-api 
+    docker tag photo-garden-search-api:latest ${REMOTE_IMAGE_URL}
+    docker push ${REMOTE_IMAGE_URL}
   else
     echo "Not master, not deploying"
   fi
