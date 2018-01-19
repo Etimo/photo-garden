28d511e8b6fbf94b2b026b46deb029d10158b893
Erik Malm <erik.malm@etimo.se>
2018-01-19 13:55:18 +0100

    Initial jsx

--- web-frontend/src/js/main.jsx
+++ web-frontend/src/js/main.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from "./header.jsx";
+import Footer from 	'./footer.jsx';
+import Login from 	'./login.jsx';
 import ReactDOM from 'react-dom';
 import main from "../sass/main.scss";
 
@@ -20,9 +23,14 @@ class Main extends React.Component {
 		super(props);
 	}
 	render() {
-		return <div>
-			<p>{this.props.prop}</p>
-		</div>
+		return (
+		<div className="wrapper">
+			<Header />
+			<main className="main">
+			<Login/>
+			</main>
+			<Footer/>
+	</div>)
 	}
 }
 const element = <Main prop="I AM A MIGHTY PROPPY PROP2!" />;
