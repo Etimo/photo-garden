263dfdc94e231d825ef11861fa8f1b9610ed2f14
JassyrBravo <31710951+JassyrBravo@users.noreply.github.com>
2018-01-19 17:41:38 +0100

    Add photos-api to docker

--- apis/photos-api/app.js
+++ apis/photos-api/app.js
@@ -1,75 +1,41 @@
 //drive-api
 var express = require('express');
-var google = require('googleapis');
-var GoogleAuth = require('google-auth-library');
-var auth = new GoogleAuth;
-var CLIENT_ID = "212991127628-8rj19c00v2d1tpl9v3rpd2vd740o6d96.apps.googleusercontent.com";
-var client = new auth.OAuth2(CLIENT_ID, '', '');
+
 const app = express();
 let accessToken;
 app.use(express.static('public'));
 
-app.post('/drive/access-token', function(req, res) {
-  accessToken = req.headers['x-auth'];
-  res.send('hello');
+
+app.get('/test', function (req, res) {
+	var jsn = {} 
+	var data = 'data';
+	jsn[data] = []; 
+	
+	
+	var photo = {
+			type: 'photo',
+			id: '1',
+			user: '1450632410296',
+			url: 'https://www.google.se/search?q=nadadores&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIx9-QneTYAhUK1ywKHX7dBvYQ_AUICigB&biw=1226&bih=688&dpr=2#imgrc=20qQ0orUe3O7PM:',
+			guid:'1asdq6',
+			mimeType:'image/jpeg'
+			
+
+	};
+	var photo2 = {
+		type: 'photo',
+		id: '3',
+		user: '145063272542',
+		url: 'https://www.google.se/search?q=nadadores&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjIx9-QneTYAhUK1ywKHX7dBvYQ_AUICigB&biw=1226&bih=688&dpr=2#imgrc=20qQ0orUe3O7PM:',
+		guid:'1450632asd496',
+		mimeType:'image/jpeg'
+	};
+	jsn[data].push(photo);
+	jsn[data].push(photo2);
+	res.send(jsn);
 });
 
-app.get('/drive/list', function (req, res) {
-	var service = google.drive('v3');
-  client.credentials = {
-    access_token: accessToken
-  };
-	service.files.list({
-		auth: client,
-		pageSize: 100,
-		fields: "nextPageToken, files"
-	}, function(err, response) {
-		if (err) {
-			console.log('The API returned an error: ' + err);
-			return;
-		}
-		var files = response.files;
-    const output = files
-      .filter(item => {
-        return item.mimeType === 'image/jpeg';
-      })
-      .map(item => `<img src="${item.thumbnailLink}">`)
-      .join('');
-    res.send(output);
-	});
 
-})
-
-
-app.get('/checktoken',function(req,res){
-
- //  console.log(client);
-
- //  console.log(client.generateAuthUrl({
- //    access_type: 'offline',
- //    scope: ['https://www.googleapis.com/auth/app.photos.readonly']
- //  }));
-
-	// client.verifyIdToken(
-	// 	key,
-	// 	CLIENT_ID,
-	// 	function(e, login) {
-	// 		var payload = login.getPayload();
-	// 		var userid = payload['sub'];
-	// 		console.log(login);
-
- //      client.getToken(key, function (err, tokens) {
- //        // Now tokens contains an access_token and an optional refresh_token. Save them.
- //        console.log('get token res', tokens, err);
- //        if (!err) {
- //          oauth2Client.setCredentials(tokens);
- //        }
- //      });
-	// 		//var domain = payload['hd'];
-	// 	});
-	res.send('I AM IMMORTAL, AND NO MAN CAN BE MY EQUAL!');
-});
 app.listen(3000);
 console.log("Listening on localhost:3000")
 
-module.exports = express();
