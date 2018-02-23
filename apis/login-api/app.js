4bf3ea5b8c7fc3377320b72fc9c5b9089833b9ed
Erik Malm <erik.malm@etimo.se>
2018-02-23 15:24:10 +0100

    Working google login and uid setting via mock

--- apis/login-api/app.js
+++ apis/login-api/app.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require("express");
+const secret = process.argv.secret ? process.argv.secret : "NotAGoodSecret";
 const path = require("path");
-const google = require("googleapis");
 const sessions = require("client-sessions");
 const GoogleAuth = require("google-auth-library");
 const auth = new GoogleAuth;
@@ -9,35 +9,73 @@ const CLIENT_ID = "212991127628-8rj19c00v2d1tpl9v3rpd2vd740o6d96.apps.googleuser
 const client = new auth.OAuth2(CLIENT_ID, '', '');
 const bodyParser = require('body-parser');
 const app = express();
+
+const asyncWrapper = (fn) =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+
+app.use(sessions({
+  cookieName: "gardenSession",
+  secret: secret,
+  duration: 15 * 60 * 1000, // Refresh
+  activeDuration: 10 * 60 * 1000,
+}));
+
 app.use(bodyParser.json());
-
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("I AM A MAN OF CONSTANT SOROOOOOOOOWWW!!!!");
+  const gardener = req.gardenSession.userIdentity;
+  if (gardener) {
+    res.sendFile(path.join(__dirname + "/public/html/main.html"));
+  } else {
+    res.redirect("/login");
+  }
 });
+
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/login.html"));
+  res.sendFile(path.join(__dirname + "/public/html/login.html"));
+});
+app.get("/logout", (req, res) => {
+  req.gardenSession.reset();
+  res.sendFile(path.join(__dirname + "/public/html/logout.html"));
 });
 
+const getUserIdentity = (verified, userIdentifier, req) => {
+  //DO A LOOKUP FOR USER IDENTITY USING USERIDENTIFIER AS A KEY!
+  //Todo: Look at actual fields.
+  const userIdentity = {
+    guid: "1111111"
+  };
+  if (userIdentity) {
+    req.gardenSession.userIdentity = userIdentity.guid;
+    return true;
+  }
+  return false;
 
-app.post("/actuallylogin", (req, res) => {
+
+};
+
+app.post("/actuallylogin", asyncWrapper(async (req, res, next) => {
   const key = req.body.idToken;
   client.verifyIdToken(
     key,
     CLIENT_ID,
     (e, login) => {
       const payload = login.getPayload();
-      console.log("LOOOOOOGGG!!! "+JSON.stringify(payload));
-      });
-  res.send("I AM IMMORTAL, AND NO MAN CAN BE MY EQUAL!");
-});
+      res.status = getUserIdentity(payload.verified, payload.email, req, res) ? 200 : 401;
+      res.send("Done");
+    });
+})
+);
 
 app.listen(3000);
-console.log("Listening on localhost:3000")
-
-const getUserIdentity = (userIdentifier) => {
-  //DO A LOOKUP FOR USER IDENTITY
-
-}
+console.log("Listening on localhost:3000");
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).json({ message: "It's dead jim" });
+});
 module.exports = express();
 
