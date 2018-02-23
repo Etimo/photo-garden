664dc9d7ba0b7368db460ce1506ca0cb8a443259
Oscar Nylander <oscar.nylander@etimo.se>
2018-02-23 13:20:35 +0100

    Removed ecs deploy things

--- push.sh
+++ push.sh
@@ -1,12 +1,13 @@
 if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    pip install --user awscli
-    export PATH=${PATH}:${HOME}/.local/bin
-    eval $(aws ecr get-login --no-include-email --region eu-west-1)
-    docker tag photo-garden-search-api:latest ${SEARCH_API_REMOTE_IMAGE_URL}
-    docker tag photo-garden-providers-api:latest ${PROVIDERS_API_REMOTE_IMAGE_URL}
-    docker push ${SEARCH_API_REMOTE_IMAGE_URL}
-    docker push ${PROVIDERS_API_REMOTE_IMAGE_URL}
+    # pip install --user awscli
+    # export PATH=${PATH}:${HOME}/.local/bin
+    # eval $(aws ecr get-login --no-include-email --region eu-west-1)
+    # docker tag photo-garden-search-api:latest ${SEARCH_API_REMOTE_IMAGE_URL}
+    # docker tag photo-garden-providers-api:latest ${PROVIDERS_API_REMOTE_IMAGE_URL}
+    # docker push ${SEARCH_API_REMOTE_IMAGE_URL}
+    # docker push ${PROVIDERS_API_REMOTE_IMAGE_URL}
+    echo no-op!
   else
     echo "Not master, not deploying"
   fi
