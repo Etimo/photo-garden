942591843d87c57ca9fdc7bde310d1d5b10e5fe9
Erik Malm <erik.malm@etimo.se>
2018-02-23 13:07:24 +0100

    Initial login page

--- apis/login-api/app.js
+++ apis/login-api/app.js
@@ -1,48 +1,43 @@
-//drive-api
-var express = require('express');
-var google = require('googleapis');
-var GoogleAuth = require('google-auth-library');
-var auth = new GoogleAuth;
-var CLIENT_ID = "212991127628-8rj19c00v2d1tpl9v3rpd2vd740o6d96.apps.googleusercontent.com";
-var client = new auth.OAuth2(CLIENT_ID, '', '');
+'use strict'
+const express = require("express");
+const path = require("path");
+const google = require("googleapis");
+const sessions = require("client-sessions");
+const GoogleAuth = require("google-auth-library");
+const auth = new GoogleAuth;
+const CLIENT_ID = "212991127628-8rj19c00v2d1tpl9v3rpd2vd740o6d96.apps.googleusercontent.com";
+const client = new auth.OAuth2(CLIENT_ID, '', '');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
-app.use(express.static('public'));
-
-app.get('/login', function(req, res) {
-	    res.sendFile(path.join(__dirname + '/login.html'));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("I AM A MAN OF CONSTANT SOROOOOOOOOWWW!!!!");
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/login.html"));
 });
 
 
-app.get('/checktoken',function(req,res){
-
- //  console.log(client);
-
- //  console.log(client.generateAuthUrl({
- //    access_type: 'offline',
- //    scope: ['https://www.googleapis.com/auth/app.photos.readonly']
- //  }));
-
-	// client.verifyIdToken(
-	// 	key,
-	// 	CLIENT_ID,
-	// 	function(e, login) {
-	// 		var payload = login.getPayload();
-	// 		var userid = payload['sub'];
-	// 		console.log(login);
-
- //      client.getToken(key, function (err, tokens) {
- //        // Now tokens contains an access_token and an optional refresh_token. Save them.
- //        console.log('get token res', tokens, err);
- //        if (!err) {
- //          oauth2Client.setCredentials(tokens);
- //        }
- //      });
-	// 		//var domain = payload['hd'];
-	// 	});
-	res.send('I AM IMMORTAL, AND NO MAN CAN BE MY EQUAL!');
+app.post("/actuallylogin", (req, res) => {
+  const key = req.body.idToken;
+  client.verifyIdToken(
+    key,
+    CLIENT_ID,
+    (e, login) => {
+      const payload = login.getPayload();
+      console.log("LOOOOOOGGG!!! "+JSON.stringify(payload));
+      });
+  res.send("I AM IMMORTAL, AND NO MAN CAN BE MY EQUAL!");
 });
+
 app.listen(3000);
 console.log("Listening on localhost:3000")
 
+const getUserIdentity = (userIdentifier) => {
+  //DO A LOOKUP FOR USER IDENTITY
+
+}
 module.exports = express();
+
