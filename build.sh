6aee5ac007d5a57d0da894971e802fdbeed4d886
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-16 13:44:35 +0100

    Attempt to deploy to kubernetes with travis :ok_hand:

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/bash
+set +uex
+
 cd ${TRAVIS_BUILD_DIR}/search-api
 docker build -t photo-garden-search-api . 
 cd ${TRAVIS_BUILD_DIR}/providers
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
+#!/usr/bin/bash
+set +uex
+
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    # ./bin/ecs-deploy.sh -r eu-west-1 -c ${CLUSTER_NAME} -n ${SEARCH_API_SERVICE_NAME} -i ${SEARCH_API_REMOTE_IMAGE_URL}:latest
-    # ./bin/ecs-deploy.sh -r eu-west-1 -c ${CLUSTER_NAME} -n ${PROVIDERS_API_SERVICE_NAME} -i ${PROVIDERS_API_REMOTE_IMAGE_URL}:latest
-    echo No-op
+    mkdir ~/.kube
+    cp kubeconfig ~/.kube
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x kubectl
+
+    ./kubectl set image deployment/search-api search-api=${SEARCH_API_REMOTE_IMAGE_URL}:latest
+    ./kubectl set image deployment/providers-api providers-api=${PROVIDERS_API_REMOTE_IMAGE_URL}:latest
   else
     echo "Not master, not deploying"
   fi
