d7b283364b923d1aac40fbd44ba8523dbaedce1c
Teo Klestrup Röijezon <teo.roijezon@etimo.se>
2018-03-16 11:02:31 +0100

    Changed the default Google redirect URI

--- providers/config/default.js
+++ providers/config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   // Config for Google Drive provider
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_CLIENT_REDIRECT_URI: process.env.GOOGLE_CLIENT_REDIRECT_URI || "http://localhost:8080/providers/google-drive/auth/start",
+  GOOGLE_CLIENT_REDIRECT_URI: process.env.GOOGLE_CLIENT_REDIRECT_URI || "http://localhost:8080/providers/google-drive/auth/finish",
   QUEUE_HOST: "queue",
   QUEUE_PORT: "5672",
 };
