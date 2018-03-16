02ad2134a34c6a86c5422d41665c50905e53f8d1
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-16 14:20:17 +0100

    pls travis

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x kubectl
 
+    sed -i "s/KUBE_CA_CERT/${KUBE_CA_CERT}/g; s/KUBE_ENDPOINT/${KUBE_ENDPOINT}/g; s/KUBE_ADMIN_CERT/${KUBE_ADMIN_CERT}/g; s/KUBE_ADMIN_KEY/${KUBE_ADMIN_KEY}/g; s/KUBE_USERNAME/${KUBE_USERNAME}/g"
     ./kubectl --kubeconfig=kubeconfig set image deployment/gateway-api gateway-api=${GATEWAY_API_REMOTE_IMAGE_URL}:latest
   else
     echo "Not master, not deploying"
