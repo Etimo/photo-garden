cf1e9bf356d37fc8f8c400a08d140644343e2900
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-16 13:55:53 +0100

    please travis

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@ set +uex
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
     mkdir ~/.kube
-    cp kubeconfig ~/.kube
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x kubectl
 
-    ./kubectl set image deployment/search-api search-api=${SEARCH_API_REMOTE_IMAGE_URL}:latest
-    ./kubectl set image deployment/providers-api providers-api=${PROVIDERS_API_REMOTE_IMAGE_URL}:latest
+    ./kubectl --kubeconfig=kubeconfig set image deployment/search-api search-api=${SEARCH_API_REMOTE_IMAGE_URL}:latest
+    ./kubectl --kubeconfig=kubeconfig set image deployment/providers-api providers-api=${PROVIDERS_API_REMOTE_IMAGE_URL}:latest
   else
     echo "Not master, not deploying"
   fi
--- push.sh
+++ push.sh
@@ -3,9 +3,6 @@ set +uex
 
 if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    pip install --user awscli
-    export PATH=${PATH}:${HOME}/.local/bin
-    eval $(aws ecr get-login --no-include-email --region eu-west-1)
     docker tag photo-garden-search-api:latest ${SEARCH_API_REMOTE_IMAGE_URL}
     docker tag photo-garden-providers-api:latest ${PROVIDERS_API_REMOTE_IMAGE_URL}
     docker push ${SEARCH_API_REMOTE_IMAGE_URL}
