9dd30be00647f4fda0ff0a1a232808b039dab45e
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-16 14:10:34 +0100

    change to account for name changes

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x kubectl
 
-    ./kubectl --kubeconfig=kubeconfig set image deployment/search-api search-api=${SEARCH_API_REMOTE_IMAGE_URL}:latest
-    ./kubectl --kubeconfig=kubeconfig set image deployment/providers-api providers-api=${PROVIDERS_API_REMOTE_IMAGE_URL}:latest
+    ./kubectl --kubeconfig=kubeconfig set image deployment/gateway-api gateway-api=${GATEWAY_API_REMOTE_IMAGE_URL}:latest
   else
     echo "Not master, not deploying"
   fi
