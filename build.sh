ee19c725e42501bd2933f6be8bacc21c73af5a4f
Oscar Nylander <oscar.nylander@etimo.se>
2018-03-16 14:45:36 +0100

    :pray:

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install --user awscli
 export PATH=${PATH}:${HOME}/.local/bin
 eval $(aws ecr get-login --no-include-email --region eu-west-1)
 
-docker pull ${GATEWAY_REMOTE_IMAGE_URL}
+docker pull ${GATEWAY_API_REMOTE_IMAGE_URL}
 
 cd ${TRAVIS_BUILD_DIR}/apis/gateway
 docker build --cache-from ${GATEWAY_REMOTE_IMAGE_URL}:latest -t photo-garden-gateway-api . 
