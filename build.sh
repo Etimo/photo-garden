1861bd5f84e93407e84eb3a10e7f2e170d9659ac
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2018-05-04 14:25:20 +0200

    Echo commands

--- build.sh
+++ build.sh
@@ -8,13 +8,11 @@ eval $(aws ecr get-login --no-include-email --region eu-west-1)
 for app in $(ls apps); do
   echo "Found app: $app"
   REMOTE_URL=${ECR_BASE}$app
+  echo "Remote url: $REMOTE_URL"
+  echo "Run cmd: docker pull $REMOTE_URL"
   docker pull $REMOTE_URL
 
   # cd ${TRAVIS_BUILD_DIR}/apps/$app
-  docker build \
-    --cache-from $REMOTE_URL:latest \
-    -t photo-garden-$app \
-    -f Dockerfile.service.development \
-    --build-arg APP_NAME $app \
-    .
+  echo "Run cmd: docker build --cache-from $REMOTE_URL:latest -t photo-garden-$app -f Dockerfile.service.development --build-arg APP_NAME $app ."
+  docker build --cache-from $REMOTE_URL:latest -t photo-garden-$app -f Dockerfile.service.development --build-arg APP_NAME $app .
 done
