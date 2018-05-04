f6ad245628d150b8cedf7a9c0e6ef893e8d60061
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2018-05-04 15:32:26 +0200

    Move entrypoint to dockerfile

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     for app in $(ls apps); do
       ./kubectl --kubeconfig=kubeconfig set image deploy/$app $app=${ECR_BASE}$app:latest
     done
+    ./kubectl apply -f apps/gateway/kubernetes.yml
   else
     echo "Not master, not deploying"
   fi
