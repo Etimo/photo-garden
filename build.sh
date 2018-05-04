a3e2e23098b4ae0cd58727f18f2c35379b22fe1a
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2018-05-04 14:12:56 +0200

    Fix travis build

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ for app in $(ls apps); do
   REMOTE_URL=${ECR_BASE}$app
   docker pull $REMOTE_URL
 
-  cd ${TRAVIS_BUILD_DIR}/apps/$app
-  docker build --cache-from $REMOTE_URL:latest -t photo-garden-$app .
+  # cd ${TRAVIS_BUILD_DIR}/apps/$app
+  docker build \
+    --cache-from $REMOTE_URL:latest \
+    -t photo-garden-$app \
+    -f Dockerfile.service.development \
+    --build-arg APP_NAME $app \
+    .
 done
