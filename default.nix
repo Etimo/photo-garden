321d6de2518f53ae433bf00c45ee0b30bc79dca3
Teo Klestrup Röijezon <teo@nullable.se>
2018-06-12 08:54:16 +0200

    Fetch yarn2nix automatically

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
 {
   pkgs ? import <nixpkgs> {},
-  yarn2nix ? import ../yarn2nix { inherit pkgs; },
+  yarn2nixSrc ? pkgs.fetchFromGitHub {
+    owner = "teozkr";
+    repo = "yarn2nix";
+    rev = "local-dependencies";
+    sha256 = "1j39kwhr078mhhyhk4fpg52aybqr33qri0hmg5zksfwbc1q7gvwh";
+  },
+  yarn2nix ? import yarn2nixSrc { inherit pkgs; },
 }:
 (import ./workspace.nix { inherit yarn2nix; }).gateway
