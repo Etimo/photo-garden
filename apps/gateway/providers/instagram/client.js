e12a10a7fb704f9fccd3db46d9755a980d1179ae
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2018-06-19 22:34:42 +0200

    More work on instagram provider

--- apps/gateway/providers/instagram/client.js
+++ apps/gateway/providers/instagram/client.js
@@ -1,20 +1,11 @@
-const axios = require("axios")
 const config = require("config")
+const api = require('instagram-node').instagram();
 
-async function getAccessToken(code) {
-  const data = {
-      client_id: config.get("providers.instagram.clientId"),
-      client_secret: config.get("providers.instagram.clientSecret"),
-      redirect_uri: config.get("providers.instagram.clientRedirectUri"),
-      grant_type: 'authorization_code',
-      code,
-  }
-  console.log(data)
-  const res = await axios.get('https://api.instagram.com/oauth/access_token', data)
-  console.log(res.data)
-  return res
-}
+api.use({
+  client_id: config.get("providers.instagram.clientId"),
+  client_secret: config.get("providers.instagram.clientSecret"),
+})
 
 module.exports = {
-  getAccessToken,
+  api
 }
--- apps/gateway/providers/instagram/controllers/authController.js
+++ apps/gateway/providers/instagram/controllers/authController.js
@@ -1,18 +1,38 @@
 const config = require("config")
-const client = require("../client")
+const api = require("../client").api
+const logger = require("logging")
+const providerUser = require("provider-user");
 
-function finishAuth() {
-
-}
+const redirectUri = config.get("providers.instagram.clientRedirectUri")
 
 module.exports.authStart = (req, res) => {
-  const clientId = config.get("providers.instagram.clientId")
-  const redirectUri = config.get("providers.instagram.clientRedirectUri")
-  const link = `https://api.instagram.com/oauth/authorize/?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`
-  res.redirect(link);
+  res.redirect(api.get_authorization_url(redirectUri));
 };
 
 module.exports.authFinish = async (req, res) => {
-  const code = await client.getAccessToken(req.query.code)
+  api.authorize_user(req.query.code, redirectUri, async (err, result) => {
+    if (err) {
+      console.log(err.body);
+      res.status(403).send("Didn't work");
+    } else {
+      // Setup user
+      console.log('Yay! Access token is ', result);
+      try {
+        userId = await providerUser.getByIdentity("Instagram", result.user.id);
+      } catch (err) {
+        logger.error(`Failed to find user identity: ${err}`);
+        return false;
+      }
+      api.use({ access_token: result.access_token });
+      api.user_media_recent(result.user.id, function(err, medias, pagination, remaining, limit) {
+      console.log(err)
+      console.log(medias)
+      console.log(pagination)
+      console.log(remaining)
+      console.log(limit)
+      });
+      res.send("OK")
+    }
+  });
 };
 
