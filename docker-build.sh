2ed883dc95aaaebe68bd137fc5a2cf2a1e9c9331
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-12 16:01:13 +0200

    Nicer progress bars for the people! (who use ./docker-build.sh)

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NIX_OUT=$(nix-build -j32 --no-out-link --arg useDocker true)
+NIX_OPTS="--arg useDocker true"
+
+# nix build shows nice progress bars, but doesn't report the final derivation path
+nix build --max-jobs 32 --no-link $NIX_OPTS "$@"
+NIX_OUT=$(nix-build --no-out-link --readonly-mode $NIX_OPTS)
 docker load -i $NIX_OUT/docker-base.tar.gz
 for img in $NIX_OUT/*.docker.tar.gz; do
   docker load -i $img &
