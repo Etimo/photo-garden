7f62f4468ed4fbdcdd3ff61ac3b2e91324d5d090
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-13 18:04:17 +0200

    Nixify Travis and deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ set +uex
 
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
+    ./docker-build.sh --arg prod true
+    ./push.sh
+
     mkdir ~/.kube
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
     chmod +x kubectl
