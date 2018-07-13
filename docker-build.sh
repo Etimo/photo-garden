7e5a525cfff5549f53c48d8c038f021ef8dfbabf
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-13 18:28:24 +0200

    Report progress when building

--- docker-build.sh
+++ docker-build.sh
@@ -5,12 +5,18 @@ set -euo pipefail
 
 NIX_OPTS="--arg useDocker true $@"
 
+echo "Building images"
 # nix build shows nice progress bars, but doesn't report the final derivation path
 if [ "$CI" != true ]; then
   nix build --max-jobs 32 --no-link $NIX_OPTS
 fi
-
 NIX_OUT=$(nix-build --no-out-link --readonly-mode $NIX_OPTS)
+
+echo "Loading base image"
 docker load -i $NIX_OUT/docker-base.tar.gz
+
+echo "Loading images"
 parallel docker load -i ::: $NIX_OUT/*.docker.tar.gz
+
+echo "Copying docker-compose.yml"
 cp --no-preserve=mode $NIX_OUT/docker-compose.yml .
