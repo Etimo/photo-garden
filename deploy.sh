43d94cab4f12f675e575e92cde455a89b341725a
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-17 17:48:59 +0200

    Run deploy.sh through Nix, log in to AWS before pushing

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
+#!/usr/bin/nix-shell
+#! nix-shell -i bash -p bash kubectl awscli
 set +uex
 
 if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   if [ "${TRAVIS_BRANCH}" == "master" ]; then
+    eval $(aws ecr get-login --no-include-email --region eu-west-1)
+
     ./docker-build.sh --arg prod true
     ./push.sh
 
     mkdir ~/.kube
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-    chmod +x kubectl
 
     sed -i "s|KUBE_CA_CERT|$KUBE_CA_CERT|g; s|KUBE_ENDPOINT|$KUBE_ENDPOINT|g; s|KUBE_ADMIN_CERT|$KUBE_ADMIN_CERT|g; s|KUBE_ADMIN_KEY|$KUBE_ADMIN_KEY|g; s|KUBE_USERNAME|$KUBE_USERNAME|g" kubeconfig
     # for app in $(ls apps); do
     #   ./kubectl --kubeconfig=kubeconfig set image deploy/$app $app=${ECR_BASE}$app:latest
     # done
-    ./kubectl --kubeconfig=kubeconfig apply -f apps/gateway/kubernetes.yml
+    kubectl --kubeconfig=kubeconfig apply -f apps/gateway/kubernetes.yml
   else
     echo "Not master, not deploying"
   fi
