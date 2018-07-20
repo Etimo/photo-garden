f679d514331b7c7e38a1bc8a9fce2886bf8d5952
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-20 15:23:59 +0200

    Deploy logging

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ set -euo pipefail
 mkdir ~/.kube
 export KUBECONFIG=$(pwd)/kubeconfig
 
-# Deploy dependencies
+echo Deploying Kube Dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+echo Deploying Dependencies
 helmfile sync
 
+echo Deploying Photo Garden
 # for app in $(ls apps); do
 #   ./kubectl set image deploy/$app $app=${ECR_BASE}$app:latest
 # done
