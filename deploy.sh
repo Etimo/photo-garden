0a82deeb6d33c5e02e8963bd70419e4723cf3036
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-20 15:14:08 +0200

    Encrypt kubeconfig instead of messing around with sed

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,16 @@
 #! nix-shell -i bash -p bash kubectl helmfile awscli -j32
 set -euo pipefail
 
-if [ "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    ./push.sh
+./push.sh
 
-    mkdir ~/.kube
+mkdir ~/.kube
+export KUBECONFIG=$(pwd)/kubeconfig
 
-    sed -i "s|KUBE_CA_CERT|$KUBE_CA_CERT|g; s|KUBE_ENDPOINT|$KUBE_ENDPOINT|g; s|KUBE_ADMIN_CERT|$KUBE_ADMIN_CERT|g; s|KUBE_ADMIN_KEY|$KUBE_ADMIN_KEY|g; s|KUBE_USERNAME|$KUBE_USERNAME|g" kubeconfig
+# Deploy dependencies
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+helmfile sync
 
-    # Deploy dependencies
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-    helmfile sync
-
-    # for app in $(ls apps); do
-    #   ./kubectl --kubeconfig=kubeconfig set image deploy/$app $app=${ECR_BASE}$app:latest
-    # done
-    kubectl --kubeconfig=kubeconfig apply -f result/kubernetes/ -R
-  else
-    echo "Not master, not deploying"
-  fi
-else
-  echo "Pull request, not deploying"
-fi
+# for app in $(ls apps); do
+#   ./kubectl set image deploy/$app $app=${ECR_BASE}$app:latest
+# done
+kubectl apply -f result/kubernetes/ -R
--- push.sh
+++ push.sh
@@ -2,16 +2,7 @@
 #! nix-shell -i bash -p bash docker awscli -j32
 set -euo pipefail
 
-if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    eval $(aws ecr get-login --no-include-email --region eu-west-1)
-    for app in $(ls apps); do
-      REMOTE_URL=${ECR_BASE}$app:$DOCKER_TAG
-      docker push $REMOTE_URL
-    done
-  else
-    echo "Not master, not deploying"
-  fi
-else
-  echo "Pull request, not deploying"
-fi
+eval $(aws ecr get-login --no-include-email --region eu-west-1)
+for app in ./apps/*; do
+  docker push ${ECR_BASE}$app:$DOCKER_TAG
+done
