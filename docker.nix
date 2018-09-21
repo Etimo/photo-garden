c51481b535bf8c0c176c09dc4ab4d5848d1c6b5b
Teo Klestrup Röijezon <teo@nullable.se>
2018-09-21 20:40:16 +0200

    Moved db-migrations to the auto-generated part of docker-compose.yml

--- docker.nix
+++ docker.nix
@@ -103,7 +103,7 @@ in rec {
           "LOG_LEVEL"
         ];
       };
-    }) apps);
+    }) (apps ++ jobs));
   };
   composeFileData = lib.recursiveUpdate composeFileBase composeFileOverrides;
   composeFile = writeText "docker-compose.yml"
