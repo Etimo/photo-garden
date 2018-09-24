bf757b6dbfd08e53568eb9f77e32b5c5aa3639ac
Teo Klestrup Röijezon <teo@nullable.se>
2018-09-24 15:55:24 +0200

    Set up Helm when deploying (but not Tiller)

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,10 @@ parallel docker push "${DOCKER_IMAGE_PREFIX}{}:$DOCKER_TAG" ::: $(ls apps jobs)
 
 echo Deploying Kube Dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+echo Deploying Helm
+helm init --client-only
+echo 'Not deploying tiller due to race conditions (run `helm init --upgrade --service-acount tiller` manually if required)'
 # Disabled due to a race condition (tiller is down for a while after `helm init`)
-# echo Deploying Helm
 # kubectl apply -f result/kubernetes/serviceaccount.tiller.yml
 # helm init --upgrade --service-account tiller
 echo Deploying Other Dependencies
