0fd0ab5aff06e61fded56516cfbff6144c60e932
Erik Malm <malm.erik@gmail.com>
2019-01-11 16:07:25 +0100

    CSS tweaks

--- apps/web-frontend/src/js/components/menu/menu-item.jsx
+++ apps/web-frontend/src/js/components/menu/menu-item.jsx
@@ -16,7 +16,7 @@ const mapDispatchToProps = (dispatch,ownProps) => {
 //State sets global state parameter, allowing toggling of SPA view. Discuss reducer action/etc with jens
 //Clearing should return state of app to base. Check state on entering component to determine if item should be highlighted.
 const MenuItem = ({text,appState,image,set,currentState}) => 
-  (<div role="button" className={currentState===appState ? "menuItemDiv selected" : "menuItemDiv"}  datastate={appState} onClick={set}> 
+  (<div role="button" className={currentState.appState == appState ? "menuItemDiv selected" : "menuItemDiv"}  datastate={appState} onClick={set}> 
     <div className={image+" menuImage"}/><span className="menuItemText">{text}</span>
   </div>);
 
